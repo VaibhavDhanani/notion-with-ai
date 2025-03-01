9b9f1c828cda7f9b30e9f926e46b317ffc4e1a32
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-03-01 13:03:56 +0530

    changes for deployment

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,7 @@
 import { User } from "./types";
 
 declare global {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface CustomJwtSessionClaims extends User {
     }
 }
